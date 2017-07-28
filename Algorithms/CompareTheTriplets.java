1f124cb98a57dc38129042c9d79a544b3a38c627
Mahitha Valluru <mahitha.valluru@gmail.com>
2017-07-28 15:05:15 -0400

    Add files via upload

--- Algorithms/CompareTheTriplets.java
+++ Algorithms/CompareTheTriplets.java
@@ -1,3 +1,44 @@
+/**
+ * Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges,
+ * awarding points on a scale from 1 to 100 for three categories: problem clarity, originality,
+ * and difficulty.
+ *
+ * We define the rating for Alice's challenge to be the triplet A = (a0, a1, a2), and the
+ * rating for Bob's challenge to be the triplet B = (b0, b1, b2).
+ *
+ * Your task is to find their comparison points by comparing a0 with b0, a1 with b1, and a2 with
+ * b2.
+ *  - If ai > bi, then Alice is awarded 1 point
+ *  - If ai < bi, then Bob is awarded 1 point
+ *  - If ai = bi, then neither person receives a point
+ *
+ * Comparison points is the total points a person earned.
+ *
+ * Given A and B, can you compare the two challenges and print their respective comparison points?
+ *
+ * Input format
+ * The first line contains 3 space-separated integers, a0, a1, and a2, describing the respective
+ * values in triplet A.
+ * The second line contains 3 space-separated integers, b0, b1, and b2, describign the respective
+ * values in triplet B.
+ *
+ * Constraints
+ * 1 <= ai <= 100
+ * 1 <= bi <= 100
+ *
+ * Output format
+ * Print two space-separated integers denoting the respective comparison points earned by Alice and
+ * Bob.
+ *
+ * Sample Input
+ * 5 6 7
+ * 3 6 10
+ *
+ * Sample Output
+ * 1 1
+ */
+
+
 import java.io.*;
 import java.util.*;
 import java.text.*;
